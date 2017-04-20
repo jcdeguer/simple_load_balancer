c4cf010bdd732dcce1745e12844646b8a7767bb4
Juan Carlos <juan.deguer@gmail.com>
2017-04-20 10:37:09 -0300

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,9 @@
-class nginx_load_balancer::config {
+class simple_load_balancer:config {
   file { '/etc/nginx/nginx.conf':
       ensure  => present,
       owner => nginx,
       group => nginx,
       mode => 0550,
-      content => template ('nginx_load_balancer/nginx.conf.erb'),
+      content => template ('simple_load_balancer/nginx.conf.erb'),
   }
 }
