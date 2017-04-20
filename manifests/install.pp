f1ff5fe9239c11b85787be47c5dbdf02b4763df6
Juan Carlos <juan.deguer@gmail.com>
2017-04-20 10:39:23 -0300

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,5 @@
-class nginx_load_balancer::install
-inherits nginx_load_balancer::params {
-  case $::osfamily {
-    'Debian': {
-      exec { 'install_nginx':
-        command => '/usr/bin/apt-get -y install nginx',
-      }
-    }
-    'RedHat': {
-      exec { 'install_nginx':
-        command => '/usr/bin/yum -y install nginx',
-      }
-    }
-  }
-}
+class simple_load_balancer::install inherits simple_load_balancer::params {
+  exec { 'install_nginx':
+    command => '/usr/bin/yum -y install nginx',
+   }
+ }
